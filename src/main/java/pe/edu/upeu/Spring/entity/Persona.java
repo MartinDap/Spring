7c8a8134dd5b5387517964fb0c6ec9ab2db14076
ZambranoGamer2002 <zambranogamerplay@gmail.com>
2022-12-06 19:01:28 -0500

    Actualizacion de Persona y Tipo Persona

--- src/main/java/pe/edu/upeu/Spring/entity/Persona.java
+++ src/main/java/pe/edu/upeu/Spring/entity/Persona.java
@@ -5,6 +5,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import lombok.Data;
 
@@ -36,4 +38,9 @@ public class Persona {
 
     @Column(name = "pers_correo")
     private String persCorreo;
+    
+    @ManyToOne //De muchos a uno
+    @JoinColumn(name = "tipe_persona")
+    private TipoPersona tipoPersona;
+
 }
